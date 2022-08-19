49d43a3001e781d41c443a43f3e197357ee8ab08
Chris Johnston <cjohnston@connected.io>
2022-08-19 10:41:44 -0400

    Adds start and pause buttons to the timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,14 @@
 import React, { useRef } from "react";
 import { Grid, IconButton, Typography } from "@mui/material";
 import ReplayIcon from "@mui/icons-material/Replay";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 import Countdown, { zeroPad } from "react-countdown";
 
 export default function Timer({ handleNext }) {
   const MICROSECONDS = 1000;
   const SECONDS = 60;
-  const MINUTES = SECONDS * MICROSECONDS
+  const MINUTES = SECONDS * MICROSECONDS;
 
   const clockRef = useRef();
 
@@ -16,12 +18,33 @@ export default function Timer({ handleNext }) {
     clockRef.current.start();
   }
 
+  function handlePlay() {
+    clockRef.current.start();
+  }
+
+  function handlePause() {
+    clockRef.current.pause();
+  }
+
   return (
     <>
       <Grid item xs={12}>
         <Typography variant="body1" fontSize="8rem" textAlign="center">
-          <Countdown date={Date.now() + 8 * MINUTES} autoStart={false} ref={clockRef}
-                     renderer={({ minutes, seconds }) => (<span>{minutes}:{zeroPad(seconds)}</span>)} />
+          <Countdown date={Date.now() + 8 * MINUTES}
+                     ref={clockRef}
+                     autoStart={false}
+                     renderer={({ minutes, seconds }) => (<span>{minutes}:{zeroPad(seconds)}</span>)}
+          />
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="body1" textAlign="center" marginTop="-50px">
+          <IconButton aria-label="play" size="large" onClick={handlePlay}>
+            <PlayArrowIcon fontSize="inherit" />
+          </IconButton>
+          <IconButton aria-label="pause" size="large" onClick={handlePause}>
+            <PauseIcon fontSize="inherit" />
+          </IconButton>
           <IconButton aria-label="replay" size="large" onClick={handleStart}>
             <ReplayIcon fontSize="inherit" />
           </IconButton>
